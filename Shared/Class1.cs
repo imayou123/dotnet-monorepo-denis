fd7a632a158239b20352c9ef3a88424f673af955
denis.troller <denis.troller@sonarsource.com>
2023-10-24 16:21:03 +0200

    Initial commit

---
+++ Shared/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace Shared;
+
+public class Class1
+{
+    public Class1(string path)
+    {
+        _stream = new FileStream(path, FileMode.Create);
+    }
+    private Stream _stream;
+}
---
+++ client2/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+object? a = null;
+
+var b = a.ToString();
